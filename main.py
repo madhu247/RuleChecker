b791bd48bbbf9f7350e420554ba5723afe06e0cb
madhu247 <madhu.ch247@gmail.com>
2018-03-24 22:11:15 -0500

    feat	: parse files and load them to a sofisticated DataStructure, dict docs	: Added sample iris test data Body	: Added new functions to parse the input files and load the data to 	  a dictionary Data Structure. Also a function to check rules against 	  cases and see whether the rules correctly classify the decision or 	  not. Some utility functions to help processing the data

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ main.py, by Madhu Chegondi, 03-23-2018
 
 import sys
 import utility
+import re
 
 def main():
     print "+----------------------------------------------------------------+"
     print "|                                                                |"
-    print "|       RULE CHECKER - PROGRAMMING PROJECT EECS 138              |"
+    print "|       RULE CHECKER - PROGRAMMING PROJECT EECS 839              |"
     print "|       Author : Madhu Chegondi                                  |"
     print "|       KUID   : m136c192                                        |"
     print "|       Created: 03/23/2017                                      |"
+    print "|                                                                |"
     print "+----------------------------------------------------------------+"
     print ""
     dataFile = raw_input("Enter Name Of DataFile >> ")
@@ -21,6 +23,26 @@ def main():
         if (dataFile):
             try:
                 dfp = open(dataFile, 'r')
+                # This Program assumes that first 2 lines of the input data filename have
+                # < a a a d >
+                # [ attribute1 attribute2 attribute3 decision ]
+                header1 = dfp.readline()
+                header2 = dfp.readline().strip().split()
+                AttNames = header2[1:-1]
+                DesName = header2[-2]
+                Cases = []
+                for line in dfp:
+                    if re.match(r'^\!.*', line) or line.strip() == '':
+                        continue
+                    values = line.split()
+                    rawData = {}
+                    for i in range(len(values)):
+                        try:
+                            if(type(float(values[i])) == float):
+                                rawData[AttNames[i]] = float(values[i])
+                        except ValueError:
+                            rawData[AttNames[i]] = values[i]
+                    Cases.append(rawData)
                 break
             except:
                 print "\n\tERROR: Enter A Valid File Name\n"
@@ -33,17 +55,34 @@ def main():
         if (rulesFile):
             try:
                 rfp = open(rulesFile, 'r')
-                if not utility.checkRuleFile(rulesFile):
+                if not utility.checkRuleFile(rulesFile)[0]:
                     print "\n\tERROR:"
                     print "\tRules In The File Are Not In Correct Format\n\tPlease Check The Rules In The File\n"
                     rulesFile = raw_input("Enter Name Of RulesFile >> ")
                 else:
+                    Rules = utility.checkRuleFile(rulesFile)[1]
                     break
             except:
                 print "\n\tERROR: Enter A Valid File Name\n"
                 rulesFile = raw_input("Enter Name Of RulesFile >> ")
         else:
             rulesFile = raw_input("Enter Name Of RulesFile >> ")
+
+    correctlyClassifiedCases = []
+    inCorrectlyClassifiedCases = []
+    for i in range(len(Cases)):
+        flag = 0
+        for j in range(len(Rules)):
+            if utility.checkRules(Rules[j], Cases[i], DesName):
+                flag = 1
+                if i not in correctlyClassifiedCases:
+                    correctlyClassifiedCases.append(i)
+        if not flag:
+            inCorrectlyClassifiedCases.append(i)
+
+    print correctlyClassifiedCases
+    print inCorrectlyClassifiedCases
+
 
 
 main()
--- utility.py
+++ utility.py
@@ -1,3 +1,8 @@
+#!usr/bin/python
+
+"""
+utility.py, by Madhu Chegondi, 03-23-2018
+"""
 import re
 
 def checkRuleFile(fileName):
@@ -5,19 +10,78 @@ def checkRuleFile(fileName):
     pattern = re.compile(r"^[0-9]+\s*,\s*[0-9]+\s*,\s*[0-9]+\s* (\( [a-zA-Z\-_\s,]+ ([0-9]+.?[0-9]* | [0-9]+.?[0-9]*..[0-9]+.?[0-9]* | [a-zA-Z\-_]+)\)\s*[\^\&]{0,1}\s*)+\-{1,2}>\s*\( [a-zA-Z\-_\s,]+ ([0-9]+.?[0-9]* | [0-9]+.?[0-9]*..[0-9]+.?[0-9]* | [a-zA-Z\-_]+)\)",re.VERBOSE)
     flag = 0
     tmpLine =''
+    arrayOfDicts = []
     for line in fp:
         if re.match(r'^\!.*', line) or line.strip() == '':
             continue
         tmpLine = tmpLine + ''.join(line)
         if pattern.match(tmpLine):
-            # tmpLine = ' '.join(tmpLine.split('\n'))
-            # print tmpLine
+            # Check for \r too Carriage Return
+            tmpLine = ' '.join(tmpLine.split('\n'))
+            arrayOfDicts.append(dictFromRules(tmpLine))
             tmpLine = ''
             flag = 1
         else:
             flag = 0
             continue
     if flag == 0:
-        return False
+        arrayOfDict = None
+        return False, arrayOfDicts
     else:
-        return True
+        return True, arrayOfDicts
+
+
+def dictFromRules(RuleString):
+    d = {}
+    start = RuleString.find('(')
+    [d['specificity'], d['strength'], d['numOfTrainCases']] = RuleString[0:start].split(',')
+    d['specificity'] = int(d['specificity'].strip())
+    d['strength'] = int(d['strength'].strip())
+    d['numOfTrainCases'] = int(d['numOfTrainCases'].strip())
+    desIndex = RuleString.rfind('(')
+    d[RuleString[desIndex+1:].split(',')[0].strip()] = RuleString[desIndex:].split(',')[1].strip().replace(')','')
+    end = RuleString.find('->')
+    if RuleString[end-1] == '-':
+        end = end - 1
+    for char in ['(', ')', '&', ',', '^']:
+        if char in RuleString[start:end]:
+            RuleString = RuleString.replace(char, ' ')
+    for i in range(0,len(RuleString[start:end].split())-1,2):
+        d[RuleString[start:end].split()[i]] = RuleString[start:end].split()[i+1]
+    return d
+
+
+def checkRules(Rules, Cases, DesName):
+    Keys = [ k for k in Rules.keys() if k not in ['specificity', 'strength', 'numOfTrainCases', DesName]]
+    flag = 0
+    for k in Keys:
+        if Cases[k] == '-' or Cases[k] == '*':
+            Cases[k] = Rules[k]
+        # Code to handle '?'
+        # elif Cases[k] == '?':
+        #     Cases[k] = 'Madhu'
+        if Rules[k] == Cases[k]:
+            flag = 1
+            continue
+        elif Rules[k].find('..'):
+            values = getValues(Rules[k])
+            if Cases[k] >= values[0] and Cases[k] <= values[1]:
+                flag = 1
+                continue
+            else:
+                break
+        else:
+            break
+    if flag:
+        if Rules[DesName] == Cases[DesName]:
+            return True
+    else:
+        return False
+
+def getValues(symNumericals):
+    if symNumericals.find('..'):
+        stingValues = symNumericals.split('..')
+    else:
+        return
+    values = [float(i) for i in stingValues]
+    return values
